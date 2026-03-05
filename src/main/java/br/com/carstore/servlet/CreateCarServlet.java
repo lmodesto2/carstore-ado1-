f0ce5b412e07686b4b7b546e1d541dec4ad44427
LUIZ FELLIPE <luiz.fmqueiroz1@senacsp.edu.br>
2026-03-04 22:59:59 -0300

    criação da classe CreateCarServlet e mapeamento da rota

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,9 @@
+package br.com.carstore.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+}
